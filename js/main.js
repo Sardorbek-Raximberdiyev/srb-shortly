73b0589a55aa13df5c4eaaf53447be9f54647f68
Sardorbek-Raximberdiyev <sardorraximberdiyev9@gmail.com>
2022-05-18 18:08:16 +0500

    Add form js style

--- js/main.js
+++ js/main.js
@@ -1,15 +1,23 @@
-const elFormButton = document.querySelector('.js-form-button');
+const elJsForm = document.querySelector('.js-form');
 const elFormCopyLinkBlock = document.querySelector('.form__copy-link-block');
-const elCopyButton = document.querySelectorAll('.copy-button');
+const elJsCopyBlock = document.querySelector('.js-copy-block');
+const elCopyButton = document.querySelector('.copy-button');
+
+elJsForm.addEventListener('submit', function (evt) {
+  evt.preventDefault();
+
+  elFormCopyLinkBlock.classList.add('form__copy-link-block-open');
+});
 
 
+elJsCopyBlock.addEventListener('click', function (evt) {
+  if (evt.target.matches('.copy-button')) {
+    evt.target.textContent = 'Copied';
 
-
-
-elFormButton.addEventListener('click', function () {
-  elFormCopyLinkBlock.classList.add('form__copy-link-block-remove');
-
-  elCopyButton.addEventListener('click', function () {
-    elCopyButton.textContent = 'Copied!';
-  });
+    evt.target.classList.add('js-copy-button');
+    setTimeout((elJsCopyBlock) => {
+      evt.target.textContent = 'Copy';
+      evt.target.classList.remove('js-copy-button');
+    }, 1000);
+  };
 });
